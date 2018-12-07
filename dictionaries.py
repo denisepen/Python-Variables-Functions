86aa40fd19e3a71aeb1cd825c7de99d448a0026a
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-12-07 14:08:31 -0500

    dictionaries -> hash/object

--- dictionaries.py
+++ dictionaries.py
@@ -1,1 +1,15 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+# like ruby hashes and js objects
+
+person = {
+'first_name': 'Dee',
+'last_name': 'Doe',
+'age': 39
+}
+
+print(person['age'])
+print(person.get('last_name'))
+
+# add key value
+person['height'] = 5.5
+print(person)
--- tuples_sets.py
+++ tuples_sets.py
@@ -1,5 +1,9 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
+
 
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+fruit_set = {'Apple', 'Orange', 'Mango'}
+
+fruit_set.add('banana')
+print(fruit_set)
